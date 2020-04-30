fc9bbd2e12a5d613674d8ec6ef7b18e39c16bf42
Artyom Gevorgyan <43173333+gevorgyana@users.noreply.github.com>
2020-04-30 21:25:17 +0300

    For completeness, add stack-related functions
    Credits to this https://gist.github.com/bmc/1323553/712a355e093f4c0f7fb05290373620e4e58f15fd

--- finders.sh
+++ finders.sh
@@ -81,6 +81,74 @@ alias grefll=$' gref | awk \' {print $2} \' | grep \'.*\..*\' | xargs less '
 
 # Todo store the state of regex backend too as sometimes it may be important
 
+# Same as the original version
+stack_exists ()
+{
+    : ${1?'Missing stack name'};
+    eval '_i=$'"_stack_$1_i";
+    if [[ -z "$_i" ]]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+# Same as the original
+no_such_stack ()
+{
+    : ${1?'Missing stack name'};
+    stack_exists $1;
+    ret=$?;
+    declare -i x;
+    let x="1-$ret";
+    return $x
+}
+
+# Same as the original
+stack_destroy
+{
+    : ${1?'Missing stack name'}
+    eval "unset _stack_$1 _stack_$1_i"
+    return 0
+}
+
+
+# Same as the original
+stack_new ()
+{
+    : ${1?'Missing stack name'};
+    if stack_exists $1; then
+        echo "Stack already exists -- $1" 1>&2;
+        return 1;
+    fi;
+    eval "declare -ag _stack_$1";
+    eval "declare -ig _stack_$1_i";
+    eval "let _stack_$1_i=0";
+    return 0
+}
+
+stack_pop ()
+{
+    : ${1?'Missing stack name'};
+    : ${2?'Missing name of variable for popped result'};
+    eval 'let _i=$'"_stack_$1_i";
+    if no_such_stack $1; then
+        echo "No such stack -- $1" 1>&2;
+        return 1;
+    fi;
+    if [[ "$_i" -eq 0 ]]; then
+        echo "Empty stack -- $1" 1>&2;
+        return 1;
+    fi;
+    let _i-=1;
+    eval "$2"='$'"{_stack_$1[$_i]}";
+    eval "unset _stack_$1[$_i]";
+    eval "_stack_$1_i=$_i";
+    unset _i;
+    return 0
+}
+
+
 # This version of push differs from the original one in that it saves 'first second'
 # as one item in the stack, whereas the original version would see it as two words
 # delimited by a space;
